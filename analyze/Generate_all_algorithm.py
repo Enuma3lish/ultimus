9f253fad9e42d9750a644cf547708c2b06e9b05a
melo <qaz0978005418@gmail.com>
2024-03-13 21:29:06 +0800

    add customize function

--- analyze/Generate_all_algorithm.py
+++ analyze/Generate_all_algorithm.py
@@ -1,6 +1,6 @@
 import Generate_average_flow_time_all_algorithm
 
-algorithmList = ["avg_flow_time_compare_with_fcfs.csv","avg_flow_time_compare_with_srpt.csv","l2norm_compare_with_fcfs.csv","l2norm_compare_with_srpt.csv","Rmlfq_l2norm_compare_with_Rr.csv"]
+algorithmList = ["avg_flow_time_compare_with_fcfs.csv","avg_flow_time_compare_with_srpt.csv","l2norm_compare_with_fcfs.csv","l2norm_compare_with_srpt.csv","Rmlfq_l2norm_compare_with_Rr.csv","avg_flow_time_compare_with_customize.csv"]
 
 for name in algorithmList:
     Generate_average_flow_time_all_algorithm.draw(name)
