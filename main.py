dde848925b809e7a99d22a64bf0830060b705578
melo <qaz0978005418@gmail.com>
2024-04-15 14:23:04 +0800

    add Mmlfq

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ num_jobs = 10000
 def main():
     #Job_init.Save_file(num_jobs)
     Arrival_rate = [i for i in range(20, 42, 2)]
-    quantum_decrease = [1,2,4,8,16,32,64,128,256,512,1024]
+    quantum_decrease = [0,1,2,4,8,16,32,64,128,256,512,1024]
     quantum_multiplier = [1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0]
     bp_parameter=[{"L":16.772,"H":pow(2,6)},{"L":7.918,"H":pow(2,9)},{"L":5.649,"H":pow(2,12)},{"L":4.639,"H":pow(2,15)},{"L":4.073,"H":pow(2,18)}]
     #for i in Arrival_rate:
@@ -17,8 +17,8 @@ def main():
     progress = tqdm.tqdm(total=len(Arrival_rate)*len(quantum_decrease)*len(quantum_multiplier))
 
     for i in Arrival_rate:
-        for j in quantum_decrease:
-            for k in quantum_multiplier:
+        for j in quantum_multiplier:
+            for k in quantum_decrease:
                 # print(i,j,k)
                 result = execute.execute(i,bp_parameter,j,k)
                 results.extend(result)
