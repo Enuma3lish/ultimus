67652981b536d07aa198dc4add17edc9e49ad0d3
melo <qaz0978005418@gmail.com>
2025-01-13 23:46:43 +0800

    modify job_init

--- main_RDYNAMIC.py
+++ main_RDYNAMIC.py
@@ -10,16 +10,24 @@ logger = logging.getLogger(__name__)
 
 def create_dataset():
     Arrival_rate = [i for i in range(20, 42, 2)]
-    check = [1,2,4,8,16,30] 
+    check = [1,2,4,8,16,30,32,64] 
     #[32,64,128,256,512,1024,2048,4096,8192,16384]
     bp_parameter = [
         {"L": 16.772, "H": pow(2, 6)},
         {"L": 7.918, "H": pow(2, 9)},
         {"L": 5.649, "H": pow(2, 12)},
         {"L": 4.639, "H": pow(2, 15)},
-        {"L": 4.073, "H": pow(2, 18)}
+        {"L": 4.073, "H": pow(2, 18)},
+        {"L": 56.300, "H": pow(2, 6)},
+        {"L": 18.900, "H": pow(2, 9)},
+        {"L": 12.400, "H": pow(2, 12)},
+        {"L": 9.800, "H": pow(2, 15)},
+        {"L": 8.500, "H": pow(2, 18)},
+        {"L": 32.300, "H": pow(2, 9)},
+        {"L": 19.700, "H": pow(2, 12)},
+        {"L": 15.300, "H": pow(2, 15)},
+        {"L": 13.000, "H": pow(2, 18)}
     ]
-
     # Create the log directory if it doesn't exist
     log_dir = "/Users/melowu/Desktop/ultimus/log/"
     os.makedirs(log_dir, exist_ok=True)
