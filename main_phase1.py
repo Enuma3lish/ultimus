bf460b8ce3e75249de250b7e607908bfb9e3359d
melo <qaz0978005418@gmail.com>
2025-01-17 16:13:45 +0800

    create phase1

--- main_phase1.py
+++ main_phase1.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 
 def create_dataset():
     Arrival_rate = [i for i in range(20, 42, 2)]
-    check = [32,64] 
     #[32,64,128,256,512,1024,2048,4096,8192,16384]
     bp_parameter = [
         {"L": 16.772, "H": pow(2, 6)},
@@ -34,11 +33,11 @@ def create_dataset():
     os.makedirs(log_dir, exist_ok=True)
     # Use tqdm for the outer loop to show overall progress
     results = []
-    for i in tqdm.tqdm(Arrival_rate, desc=f"Processing arrival rates for checkpoint {c}", leave=False):
+    for i in tqdm.tqdm(Arrival_rate, desc=f"Processing arrival rates", leave=False):
         try:
             execute_phase1.execute_phase1(i, bp_parameter)
         except Exception as e:
-                logger.error(f"Error processing arrival rate {i} for checkpoint {c}: {e}")
+                logger.error(f"Error processing arrival rate {i} : {e}")
                 continue
 
 if __name__ == "__main__":
